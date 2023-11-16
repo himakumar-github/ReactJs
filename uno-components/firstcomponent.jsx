c6d01b8bc651b803ab989d289cf8521a40c33382
Himakumar Tellaboina <139366126+himakumar-github@users.noreply.github.com>
2023-11-16 10:47:07 +0530

    Delete uno-components/firstcomponent.jsx

--- uno-components/firstcomponent.jsx
+++
@@ -1,9 +0,0 @@
-
-import React, { Component } from 'react';
-class Firstcomponent extends Component {
-    state = {  } 
-    render() { 
-        return (<h1>My first Component</h1>);
-    }
-}
-export default Firstcomponent;
