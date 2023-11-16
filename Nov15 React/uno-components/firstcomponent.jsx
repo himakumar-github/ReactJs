6a602759ae791453edcef25d8a19f8fd1f91e39e
Himakumar Tellaboina <139366126+himakumar-github@users.noreply.github.com>
2023-11-16 10:49:08 +0530

    Add files via upload

---
+++ Nov15 React/uno-components/firstcomponent.jsx
@@ -0,0 +1,9 @@
+
+import React, { Component } from 'react';
+class Firstcomponent extends Component {
+    state = {  } 
+    render() { 
+        return (<h1>My first Component</h1>);
+    }
+}
+export default Firstcomponent;
