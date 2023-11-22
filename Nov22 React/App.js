a37fc514dd9c481aa7b7946b5ccb173f62bb89d3
Himakumar Tellaboina <139366126+himakumar-github@users.noreply.github.com>
2023-11-22 12:28:37 +0530

    Add files via upload

---
+++ Nov22 React/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import MapComponents from './Uno-components/MapComponents';
+
+function App() {
+  return (
+   <MapComponents/>
+  );
+}
+export default App;
