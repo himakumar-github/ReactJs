e19e1c46fb731232f86bfcfb864bf6d22ddc2d69
Himakumar Tellaboina <139366126+himakumar-github@users.noreply.github.com>
2023-11-23 12:27:45 +0530

    Add files via upload

---
+++ Nov23 React/Uno-Components/MyComponents.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+class MyComponents extends Component {
+    state = { 
+        counter:0
+     } 
+     pressButton=()=>{
+        this.setState({counter:this.state.counter+1})
+        console.log("Button Pressed!")
+     }
+     incrementButton=()=>{
+        this.setState({counter:this.state.counter+1})
+     }
+     decrementButton=()=>{
+        this.setState({counter:this.state.counter-1})
+     }
+    render() {
+        return (
+            <div>
+                <span className='badge bg-warning m-4'>{this.state.counter}</span>
+                <button className='btn btn-primary m-4' onClick={this.incrementButton}>+</button>
+                <button className='btn btn-primary m-4' onClick={this.decrementButton}>-</button>
+            </div>
+        );
+    }
+}
+
+export default MyComponents;
